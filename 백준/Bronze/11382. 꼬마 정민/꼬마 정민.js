6f9f36b82815679e085bf25d79c79cc0a73228c5
Geon Kim <49513356+funfungun@users.noreply.github.com>
2025-06-04 23:34:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const line = input[0].split(" ");
+
+const A = parseInt(line[0]);
+const B = parseInt(line[1]);
+const C = parseInt(line[2]);
+
+console.log(A + B + C);
