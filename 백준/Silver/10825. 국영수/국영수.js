29e2e3a20a27d2b8b8211484487adebd20cafe45
Geon Kim <49513356+funfungun@users.noreply.github.com>
2025-06-23 11:22:04 +0900

    [Silver IV] Title: 국영수, Time: 3716 ms, Memory: 68824 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.js
+++ 백준/Silver/10825. 국영수/국영수.js
@@ -3,7 +3,6 @@ const input = fs.readFileSync(0, 'utf-8').toString().trim().split("\n");
 
 const N = Number(input[0]);
 let arr = [];
-
 for (let i = 1; i <= N; i++) {
   const line = input[i].split(" ");
   arr.push([line[0], Number(line[1]), Number(line[2]), Number(line[3])]);
@@ -12,19 +11,18 @@ for (let i = 1; i <= N; i++) {
 arr.sort((a, b) => {
   if (a[1] !== b[1]) {
     return b[1] - a[1];
-  }
-  if (a[2] !== b[2]) {
+  } else if (a[2] !== b[2]) {
     return a[2] - b[2];
-  }
-  if (a[3] !== b[3]) {
+  } else if (a[3] !== b[3]) {
     return b[3] - a[3];
-  }
-  if (a[0] < b[0]) {
-    return -1;
-  } else if (a[0] > b[0]) {
-    return 1;
   } else {
-    return 0;
+    if (a[0] < b[0]) {
+      return -1;
+    } else if (a[0] > b[0]) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 });
 
