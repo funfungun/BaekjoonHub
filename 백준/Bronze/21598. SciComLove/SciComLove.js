85a29fe41fd1ede8a7b3d56c73e9b4764cff5af6
Geon Kim <49513356+funfungun@users.noreply.github.com>
2025-06-24 19:24:09 +0900

    [Bronze V] Title: SciComLove, Time: 96 ms, Memory: 9324 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const N = Number(fs.readFileSync(0, 'utf-8').toString().trim());
+
+if (N === 1) {
+  console.log("SciComLove");
+} else if (N === 2) {
+  console.log("SciComLove");
+  console.log("SciComLove");
+}
