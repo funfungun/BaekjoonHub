46f927e5051b9e65fcb622089168d1f31c94adb9
Geon Kim <49513356+funfungun@users.noreply.github.com>
2025-06-24 19:45:43 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 88 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const id = fs.readFileSync(0, 'utf-8').toString().trim();
+
+console.log(":fan::fan::fan:");
+console.log(`:fan::${id}::fan:`);
+console.log(":fan::fan::fan:");
