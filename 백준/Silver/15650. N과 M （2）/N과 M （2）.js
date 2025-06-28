f368b2f52b1f17c5ac8798eefaced584ee8fc85f
Geon Kim <49513356+funfungun@users.noreply.github.com>
2025-06-28 18:19:06 +0900

    [Silver III] Title: N과 M (2), Time: 92 ms, Memory: 9392 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const [N, M] = fs
+  .readFileSync(0, 'utf-8')
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const output = [];
+const backtracking = (start, arr) => {
+  if (arr.length === M) {
+    output.push(arr.join(" "));
+    return;
+  }
+
+  for (let i = start; i <= N; i++) {
+    backtracking(i + 1, [...arr, i]);
+  }
+};
+
+backtracking(1, []);
+console.log(output.join("\n"));
