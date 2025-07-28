d35cb573d6f28551a21689cfcb9cfa49c03546ef
Geon Kim <49513356+funfungun@users.noreply.github.com>
2025-07-28 18:31:25 +0900

    [Bronze V] Title: Adding Trouble, Time: 96 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const [A, B, C] = fs
+  .readFileSync(0, 'utf-8')
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+console.log(A + B == C ? "correct!" : "wrong!");
